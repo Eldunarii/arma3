bd7e9710ad2071b320d2c21089371a0b742bb8b2
Bastien <troubatbastien@gmail.com>
2023-03-22 16:34:43 +0100

    test

--- html_to_ids.sh
+++ html_to_ids.sh
@@ -20,6 +20,6 @@ output_file="mod_names.txt"
 while read id; do
     name=$(../steamcmd.sh +login pouletopipo cAssjZ23~~aa +workshop_download_item 107410 "$id" validate +quit | grep "Name:" | cut -d: -f2 | tr -d '[:space:]')
     echo "$name" >> "$output_file"
-done < "$input_file"
+done
 
 echo "Mod names saved to $output_file"
