8d69b0eca54d6325c15b81e23e97ab582a1b55e6
Bastien <troubatbastien@gmail.com>
2023-03-22 15:31:26 +0100

    test

--- html_to_ids.sh
+++ html_to_ids.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the HTML file path
-html_file="modsHTML/mods.html"
+html_file="modsHTML/Test_Serveur_Braquage.html"
 
 # Use grep to extract the mod IDs from the URLs
 grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 > mod_ids.txt
