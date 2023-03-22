009f3a1a87b2b090410bd489645a29c9fb9c7e98
Bastien <troubatbastien@gmail.com>
2023-03-22 16:09:34 +0100

    test

--- html_to_ids.sh
+++ html_to_ids.sh
@@ -14,7 +14,7 @@ grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 | sort -u > "$id_output_file"
 
 # Loop through each ID and get the name using SteamCMD
 while read id; do
-    name=$(./steamcmd.sh +login username password +workshop_download_item 107410 "$id" validate +quit | grep "Name:" | cut -d: -f2 | tr -d '[:space:]')
+    name=$(../steamcmd.sh +login username password +workshop_download_item 107410 "$id" validate +quit | grep "Name:" | cut -d: -f2 | tr -d '[:space:]')
     echo "$name" >> "$name_output_file"
 done < "$id_output_file"
 
