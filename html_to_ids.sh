a4a58ea5fb6499ddbc476ffa18200659dbb5bc19
Bastien <troubatbastien@gmail.com>
2023-03-22 16:05:24 +0100

    test

--- html_to_ids.sh
+++ html_to_ids.sh
@@ -3,22 +3,19 @@
 # Define the HTML file path
 html_file="modsHTML/Test_Serveur_Braquage.html"
 
-# Define the output file path
-output_file=$(basename -s .html "$html_file")_Preset.txt
+# Define the output file path for the IDs
+id_output_file=$(basename -s .html "$html_file")_IDs.txt
+
+# Define the output file path for the names
+name_output_file=$(basename -s .html "$html_file")_Names.txt
 
 # Use grep to extract the mod IDs from the URLs, remove duplicates and sort
-grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 | sort -u > "$output_file"
+grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 | sort -u > "$id_output_file"
 
-echo "Mod IDs saved to $output_file"
+# Loop through each ID and get the name using SteamCMD
+while read id; do
+    name=$(./steamcmd.sh +login username password +workshop_download_item 107410 "$id" validate +quit | grep "Name:" | cut -d: -f2 | tr -d '[:space:]')
+    echo "$name" >> "$name_output_file"
+done < "$id_output_file"
 
-id_file=$(basename -s .html "$html_file")_IDs.txt
-name_file=$(basename -s .html "$html_file")_Names.txt
-
-# Use grep to extract the mod IDs and names from the URLs, remove duplicates and sort
-grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 | sort -u > "$id_file"
-grep -o "title=\"[^\"]*" "$html_file" | cut -d= -f2- | sed 's/\"//g' > "$name_file"
-
-# Combine the IDs and names into a single file
-paste "$id_file" "$name_file" > "$(basename -s .txt "$id_file")_with_names.txt"
-
-echo "Mod IDs and names saved to $(basename -s .txt "$id_file")_with_names.txt"
+echo "Mod names saved to $name_output_file"
