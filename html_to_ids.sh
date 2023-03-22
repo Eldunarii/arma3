e282ecc95a471a1319d6fe3c544594dae7339584
Bastien <troubatbastien@gmail.com>
2023-03-22 15:59:43 +0100

    test

--- html_to_ids.sh
+++ html_to_ids.sh
@@ -10,3 +10,15 @@ output_file=$(basename -s .html "$html_file")_Preset.txt
 grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 | sort -u > "$output_file"
 
 echo "Mod IDs saved to $output_file"
+
+
+name_file=$(basename -s .html "$html_file")_Names.txt
+
+# Use grep to extract the mod IDs and names from the URLs, remove duplicates and sort
+grep -o "id=[0-9]*" "$html_file" | cut -d= -f2 | sort -u > "$id_file"
+grep -o "title=\"[^\"]*" "$html_file" | cut -d= -f2- | sed 's/\"//g' > "$name_file"
+
+# Combine the IDs and names into a single file
+paste "$id_file" "$name_file" > "$(basename -s .txt "$id_file")_with_names.txt"
+
+echo "Mod IDs and names saved to $(basename -s .txt "$id_file")_with_names.txt"
